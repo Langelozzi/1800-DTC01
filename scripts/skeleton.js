215c6a009e99f5fb7f10b71ff753e14b9dd226dc
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-10-29 16:38:11 -0700

    Add nav and footer to skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,4 +1,6 @@
 function loadSkeleton() {
     $('.inbox-card-placeholder').load('/templates/inbox-card.html');
+    $('#navbarPlaceholder').load('./text/nav.html');
+    $('#footerPlaceholder').load('./text/footer.html');
 }
 loadSkeleton();
