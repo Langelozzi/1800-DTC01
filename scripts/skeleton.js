7dffbe11b4ece3294b9a47b220cfc538cc88acba
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-10-29 20:03:57 -0700

    Add nav-back-btn.html to skeleton

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,6 +1,7 @@
 function loadSkeleton() {
     $('.inbox-card-placeholder').load('/templates/inbox-card.html');
     $('#navbarPlaceholder').load('./text/nav.html');
+    $('#navbar-back-btnPlaceholder').load('./text/nav-back-btn.html');
     $('#footerPlaceholder').load('./text/footer.html');
 }
 loadSkeleton();
