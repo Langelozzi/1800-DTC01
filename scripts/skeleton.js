8e1d54394bc12988e2710baccb732e223fc53d70
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-02 12:21:37 -0700

    move text files to templates folder

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,8 +1,8 @@
 function loadSkeleton() {
     $('.inbox-card-placeholder').load('/templates/inbox-card.html');
-    $('#navbarPlaceholder').load('./text/nav.html');
-    $('#navbar-back-btnPlaceholder').load('./text/nav-back-btn.html');
-    $('#footerPlaceholder').load('./text/footer.html');
+    $('#navbarPlaceholder').load('./templates/nav.html');
+    $('#navbar-back-btnPlaceholder').load('./templates/nav-back-btn.html');
+    $('#footerPlaceholder').load('./templates/footer.html');
     $('.browse-card-placeholder').load('/templates/browse-card.html');
     $('.detail-card-placeholder').load('/templates/compliment-detail-card.html');
 }
