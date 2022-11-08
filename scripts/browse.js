e360b1c90bfd57c701bec0face452d6ac3b652ed
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-07 20:32:11 -0800

    finish dynamic compliment population

--- scripts/browse.js
+++ scripts/browse.js
@@ -1,9 +1,32 @@
+function loadComplimentCards() {
+    var template = document.getElementById('browse-card-template');
+
+    db.collection('compliments').get().then((data) => {
+        data.forEach(element => {
+            let complimentData = element.data();
+            let complimentId = element.id;
+            let complimentText = complimentData.compliment;
+            let complimentType = complimentData.type;
+
+            var clone = template.content.cloneNode(true);
+
+            clone.querySelector('.compliment-text').innerHTML = `"${complimentText}"`;
+            clone.querySelector('.compliment-type').innerHTML = complimentType;
+            clone.querySelector('.select-btn').setAttribute('href', `../compliment-details.html?complimentId=${complimentId}`);
+            console.log(clone);
+
+            $('#browse-card-list').append(clone);
+            // $('#browse-card-list').appendTo($newCard);
+        });
+    });
+}
+
 function dropDown() {
     document.getElementById("dropdownContent").classList.toggle("show");
 }
 
 //close dropdown menu
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.dropdown-toggle')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -15,3 +38,9 @@ window.onclick = function(event) {
         }
     }
 }
+
+function setUp() {
+    loadComplimentCards();
+}
+
+$(document).ready(setUp);
