95a1f473fe61440986c3b0b6b18fe249e7b7bdd0
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-08 21:33:40 -0800

    complete inbox population

--- scripts/inbox.js
+++ scripts/inbox.js
@@ -1,62 +1,72 @@
+var userId;
 
-
-function populateInboxData(messagesRef) {
-    db.collection('messages').doc(messagesRef).get()
-    const inboxData = data.data();
-
-    let inboxMessages = [];
-
-    for (let i = 0; i < inboxData.length; i++) {
-
-        if (inboxData[i].receiverId == user.uid) {
-
-            $('#inbox-compliment-text').html(`"${inboxData.complimentId.compliment}"`);
-            $('#inbox-send-at').html(inboxData.sendAt);
-        } else {
-            console.log("no messages to this user")
-        }
-    }
-
-}
-
-function receiveMessage() {
-
-    // get current date
-    const currentDate = firebase.firestore.Timestamp.now();
+function populateInboxData() {
+    const messagesRef = db.collection('messages');
+    const complimentRef = db.collection('compliments');
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            populateInboxData(messagesRef)
-            // .then((messagesRef) => {
-            //     // add opened time to message
-            //     // add one to users number of compliments received
-            //     db.collection("messages").doc(messagesRef).update({
-            //         openedAt: firebase.firestore.FieldValue.update(currentDate)
-            //     });
+            userId = user.uid;
 
-            //     db.collection("users").doc(user.uid).update({
-            //         complimentsReceived: firebase.firestore.FieldValue.increment(1)
-            //     });
+            messagesRef.get().then((data) => {
+                data.forEach(message => {
+                    let messageData = message.data();
 
-            // })
-            // .catch((error) => {
-            //     console.error("Error adding new message document: ", error);
-            // })
+                    if (messageData.receiverId == userId) {
+                        complimentRef.doc(messageData.complimentId).get().then((compliment) => {
+                            let messageText = compliment.data().compliment;
+                            let messageSentAtDate = messageData.sendAt.toDate().toDateString();
+
+                            var template = document.getElementById('inbox-card-template');
+                            var clone = template.content.cloneNode(true);
+
+                            clone.querySelector('#inbox-compliment-text').innerHTML = `"${messageText}"`;
+                            clone.querySelector('#inbox-send-at').innerHTML = messageSentAtDate;
+
+                            $('#inbox-card-list').append(clone);
+                        })
+
+                    }
+                });
+            })
         } else {
             console.log("no user");
         }
     });
-
 }
 
+// function receiveMessage() {
+
+//     // get current date
+//     const currentDate = firebase.firestore.Timestamp.now();
+
+//     firebase.auth().onAuthStateChanged(function (user) {
+//         if (user) {
+//             populateInboxData(messagesRef)
+//             .then((messagesRef) => {
+//                 // add opened time to message
+//                 // add one to users number of compliments received
+//                 db.collection("messages").doc(messagesRef).update({
+//                     openedAt: firebase.firestore.FieldValue.update(currentDate)
+//                 });
+
+//                 db.collection("users").doc(user.uid).update({
+//                     complimentsReceived: firebase.firestore.FieldValue.increment(1)
+//                 });
+
+//             })
+//             .catch((error) => {
+//                 console.error("Error adding new message document: ", error);
+//             })
+//         } else {
+//             console.log("no user");
+//         }
+//     });
+
+// }
+
 function setUp() {
-    // get message id from db
-    const urlParams = new URLSearchParams(window.location.search);
-    const senderId = urlParams.get('senderId');
-    const complimentId = urlParams.get('complimentId');
-
-    populateInboxData(user.uid, senderId, complimentId);
-
+    populateInboxData();
 }
 
 $(document).ready(setUp);
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,9 +1,6 @@
 function loadSkeleton() {
-    $('.inbox-card-placeholder').load('/templates/inbox-card.html');
     $('#navbarPlaceholder').load('./templates/nav.html');
     $('#navbar-back-btnPlaceholder').load('./templates/nav-back-btn.html');
     $('#footerPlaceholder').load('./templates/footer.html');
-    // $('.browse-card-placeholder').load('/templates/browse-card.html');
-    // $('.detail-card-placeholder').load('/templates/compliment-detail-card.html');
 }
 loadSkeleton();
