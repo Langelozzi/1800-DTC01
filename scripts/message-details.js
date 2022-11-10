072833ba10e0d109e8d7b819dc4967f31a71bbf7
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-09 20:29:23 -0800

    add chain appending logic for emoji message

--- scripts/message-details.js
+++ scripts/message-details.js
@@ -14,20 +14,34 @@ function createNewMessageDocument(senderId, receiverId, emojiId) {
     })
 }
 
-function addMessageToChain(messageId) {
-
+function addMessageToChain(chainId, messageId) {
+    // Atomically add a new message to the "messages" array field
+    return db.collection("chains").doc(chainId).update({
+        messages: firebase.firestore.FieldValue.arrayUnion(messageId)
+    })
 }
 
 function sendEmoji() {
     const selectedEmojiId = $('input[name="emoji-selection"]:checked').attr('id');
     // will change this to grabbing from the senderId of the message being responded to
     const receiverId = 'fillerRecieverId';
+    // will change this to grabbing from the chainId of the message being responded to
+    const chainId = 'tueFnMNio3QtfKpx3xzW';
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             createNewMessageDocument(user.uid, receiverId, selectedEmojiId)
                 .then((newMessageRef) => {
-                    addMessageToChain(chainId, newMessageRef.id);
+                    addMessageToChain(chainId, newMessageRef.id)
+                        .then(() => {
+                            // add the chain id to the message
+                            db.collection("messages").doc(newMessageRef.id).update({
+                                chainId: chainId
+                            });
+                        })
+                        .catch((error) => {
+                            console.log("Error adding new emoji message (emoji) to chain array: ", error);
+                        });
                 })
                 .catch((error) => {
                     console.error("Error adding new message (emoji) document: ", error);
