0fdb529f20b79d4bdcbc3b6ce81b7d00405265bc
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-09 20:35:45 -0800

    create helper function for generating emoji data

---
+++ helpers/createEmojiData.js
@@ -0,0 +1,40 @@
+function generateEmojiData() {
+    const emojisRef = db.collection("emojis");
+
+    emojisRef.doc("128522").set({
+        emojiDesc: "smile",
+        amountSent: 0
+    });
+    emojisRef.doc("128147").set({
+        emojiDesc: "heart",
+        amountSent: 0
+    });
+    emojisRef.doc("128516").set({
+        emojiDesc: "big smile",
+        amountSent: 0
+    });
+    emojisRef.doc("128518").set({
+        emojiDesc: "laughter",
+        amountSent: 0
+    });
+    emojisRef.doc("128525").set({
+        emojiDesc: "heart eyes",
+        amountSent: 0
+    });
+    emojisRef.doc("128523").set({
+        emojiDesc: "tongue out",
+        amountSent: 0
+    });
+    emojisRef.doc("128524").set({
+        emojiDesc: "satisfied",
+        amountSent: 0
+    });
+    emojisRef.doc("128526").set({
+        emojiDesc: "sunglasses",
+        amountSent: 0
+    });
+    emojisRef.doc("128079").set({
+        emojiDesc: "clapping",
+        amountSent: 0
+    });
+}
