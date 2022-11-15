9f0e61c02f1f1b8f38dee8f24c05aae944f58ea3
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-14 20:44:23 -0800

    Add nav bar with no account icon for login page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,6 +1,7 @@
 function loadSkeleton() {
     $('#navbarPlaceholder').load('./templates/nav.html');
     $('#navbar-back-btnPlaceholder').load('./templates/nav-back-btn.html');
+    $('#navbar-no-account-iconPlaceholder').load('./templates/nav-no-account-icon.html');
     $('#footerPlaceholder').load('./templates/footer.html');
 }
 loadSkeleton();
