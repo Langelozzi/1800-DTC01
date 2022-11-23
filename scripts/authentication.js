8298ca3e4323555e5d1d512acf369974f96d47c2
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-23 11:35:40 -0800

    refactor auth.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -17,14 +17,14 @@ var uiConfig = {
                     emojisSent: 0,
                     city: null,
                     country: null,
-                }).then(function () {
+                }).then(() => {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
-                })
-                    .catch(function (error) {
-                        console.log("Error adding new user: " + error);
-                    });
-            } else {
+                }).catch((error) => {
+                    console.log("Error adding new user: " + error);
+                });
+            }
+            else {
                 return true;
             }
             return false;
@@ -39,18 +39,8 @@ var uiConfig = {
     signInFlow: 'popup',
     signInSuccessUrl: 'main.html',
     signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
-    // Terms of service url.
-    // tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    // privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
 ui.start('#firebaseui-auth-container', uiConfig);
