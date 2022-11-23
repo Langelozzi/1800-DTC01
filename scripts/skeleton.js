03bb1dfba7dece8f6bf7b59003cc8cd76a14fe7a
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-23 12:12:19 -0800

    update js imports to html files

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,8 +1,8 @@
 function loadSkeleton() {
-    $('#navbarPlaceholder').load('./templates/nav.html');
-    $('#navbar-back-btnPlaceholder').load('./templates/nav-back-btn.html');
-    $('#navbar-no-account-iconPlaceholder').load('./templates/nav-no-account-icon.html');
-    $('#footerEmptyPlaceholder').load('./templates/footer-empty.html');
-    $('#footerPlaceholder').load('./templates/footer.html');
+    $('#navbarPlaceholder').load('../templates/nav.html');
+    $('#navbar-back-btnPlaceholder').load('../templates/nav-back-btn.html');
+    $('#navbar-no-account-iconPlaceholder').load('../templates/nav-no-account-icon.html');
+    $('#footerEmptyPlaceholder').load('../templates/footer-empty.html');
+    $('#footerPlaceholder').load('../templates/footer.html');
 }
 loadSkeleton();
