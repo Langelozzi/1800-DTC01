8d613fd68e76b9767d3d81f5439583ebc0a7b4ef
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-11-23 14:15:22 -0800

    update compliment-details to use async/await

--- scripts/compliment-details.js
+++ scripts/compliment-details.js
@@ -8,27 +8,26 @@ function chooseReceiver(userId, type) {
     return usersRef.get().then((querySnapshot) => {
         let allUserIds = [];
         let backupUserIds = [];
+
         querySnapshot.forEach((doc) => {
             allUserIds.push([doc.id, doc.data().preferredComplimentType]);
             backupUserIds.push([doc.id, doc.data().preferredComplimentType]);
         });
-        console.log("Type is:" + type);
+
         if (allUserIds.length == 1) {
             return allUserIds[0];
-        } else {
+        }
+        else {
             // remove sender's id from list
             let possibleReceiverIds = allUserIds;
 
             for (let i = 0; i < allUserIds.length; i++) {
                 if (possibleReceiverIds[i][1] != type && possibleReceiverIds[i][1] != null && possibleReceiverIds[i][1] != "none") {
-                    console.log(possibleReceiverIds[i][1]);
                     possibleReceiverIds.splice(i, 1);
-                    console.log("Removed " + allUserIds[i][0] + " from possible receivers");
                 }
             }
 
             if (possibleReceiverIds.length <= 1) {
-                console.log("Using backup Ids");
                 possibleReceiverIds = backupUserIds;
 
                 for (let i = 0; i < backupUserIds.length; i++) {
@@ -36,7 +35,8 @@ function chooseReceiver(userId, type) {
                         possibleReceiverIds.splice(i, 1);
                     }
                 }
-            } else {
+            }
+            else {
                 for (let i = 0; i < allUserIds.length; i++) {
                     if (possibleReceiverIds[i][0] == userId) {
                         possibleReceiverIds.splice(i, 1);
@@ -83,78 +83,81 @@ function createNewMessageDocument(senderId, receiverId, complimentId) {
     })
 }
 
-function sendMessage(complimentId, type) {
+async function sendMessage(complimentId, type) {
     // get current user from firebase auth
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
             // get random user id from firestore users collection
-            chooseReceiver(user.uid, type).then((receiverId) => {
-                createNewMessageDocument(user.uid, receiverId, complimentId)
-                    .then((newMessageRef) => {
-                        // add one to users number of compliments sent
-                        db.collection("users").doc(user.uid).update({
-                            complimentsSent: firebase.firestore.FieldValue.increment(1)
-                        });
-                        // add one to amountSent for that compliment
-                        db.collection("compliments").doc(complimentId).update({
-                            amountSent: firebase.firestore.FieldValue.increment(1)
-                        });
+            const receiverId = await chooseReceiver(user.uid, type)
 
-                        createNewChainDocument(user.uid, newMessageRef.id)
-                            .then((newChainRef) => {
-                                // add one to users number of chains started
-                                db.collection("users").doc(user.uid).update({
-                                    chainsStarted: firebase.firestore.FieldValue.increment(1)
-                                });
+            try {
+                var newMessageRef = await createNewMessageDocument(user.uid, receiverId, complimentId);
+            }
+            catch (error) {
+                console.error("Error adding new message (compliment) document: ", error);
 
-                                // add the chain id to the message
-                                db.collection("messages").doc(newMessageRef.id).update({
-                                    chainId: newChainRef.id
-                                });
+                // open error modal
+                $('#error-modal').modal('show');
+                setTimeout(() => {
+                    $('#error-modal').modal('hide');
+                }, 4000);
+            }
 
-                                // open success modal and redirect to browse page
-                                $('#success-modal').modal('show');
-                                setTimeout(() => {
-                                    $('#success-modal').modal('hide');
-                                    window.location.href = "browse.html";
-                                }, 4000);
+            // add one to users number of compliments sent
+            db.collection("users").doc(user.uid).update({
+                complimentsSent: firebase.firestore.FieldValue.increment(1)
+            });
+            // add one to amountSent for that compliment
+            db.collection("compliments").doc(complimentId).update({
+                amountSent: firebase.firestore.FieldValue.increment(1)
+            });
 
-                            }).catch((error) => {
-                                console.error("Error adding new chain document: ", error);
+            try {
+                var newChainRef = await createNewChainDocument(user.uid, newMessageRef.id);
+            }
+            catch (error) {
+                console.error("Error adding new chain document: ", error);
 
-                                // open error modal
-                                $('#error-modal').modal('show');
-                                setTimeout(() => {
-                                    $('#error-modal').modal('hide');
-                                }, 4000);
-                            });;
+                // open error modal
+                $('#error-modal').modal('show');
+                setTimeout(() => {
+                    $('#error-modal').modal('hide');
+                }, 4000);
+            }
 
-                    })
-                    .catch((error) => {
-                        console.error("Error adding new message (compliment) document: ", error);
+            // add one to users number of chains started
+            db.collection("users").doc(user.uid).update({
+                chainsStarted: firebase.firestore.FieldValue.increment(1)
+            });
 
-                        // open error modal
-                        $('#error-modal').modal('show');
-                        setTimeout(() => {
-                            $('#error-modal').modal('hide');
-                        }, 4000);
-                    });
-            })
+            // add the chain id to the message
+            db.collection("messages").doc(newMessageRef.id).update({
+                chainId: newChainRef.id
+            });
 
-        } else {
+            // open success modal and redirect to browse page
+            $('#success-modal').modal('show');
+            setTimeout(() => {
+                $('#success-modal').modal('hide');
+                window.location.href = "browse.html";
+            }, 4000);
+        }
+        else {
             console.log("no user");
+
+            // redirect to login page if no user is logged in
+            window.location.href = "../html/login.html";
         }
     });
 
 }
 
-function populateComplimentData(complimentId) {
-    db.collection('compliments').doc(complimentId).get().then((data) => {
-        const complimentData = data.data();
+async function populateComplimentData(complimentId) {
+    const data = await db.collection('compliments').doc(complimentId).get()
+    const complimentData = data.data();
 
-        $('#compliment-text').html(`"${complimentData.compliment}"`);
-        $('#compliment-type').html(complimentData.type);
-    })
+    $('#compliment-text').html(`"${complimentData.compliment}"`);
+    $('#compliment-type').html(complimentData.type);
 }
 
 function setUp() {
@@ -164,12 +167,11 @@ function setUp() {
 
     populateComplimentData(complimentId);
 
-    $('#send-btn').click(() => {
-        db.collection('compliments').doc(complimentId).get().then((data) => {
-            const complimentData = data.data();
+    $('#send-btn').click(async () => {
+        const data = await db.collection('compliments').doc(complimentId).get()
+        const complimentData = data.data();
 
-            sendMessage(complimentId, complimentData.type);
-        });
+        sendMessage(complimentId, complimentData.type);
     });
 }
 
