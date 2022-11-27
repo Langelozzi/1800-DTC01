95156116f00903f57c1998ad137160fbbe62cc74
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-26 22:45:49 -0800

    Refactor footer-empty-placeholder convention

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,7 +2,7 @@ function loadSkeleton() {
     $('#navbar-placeholder').load('../templates/nav.html');
     $('#navbar-back-btn-placeholder').load('../templates/nav-back-btn.html');
     $('#navbar-no-account-iconPlaceholder').load('../templates/nav-no-account-icon.html');
-    $('#footerEmptyPlaceholder').load('../templates/footer-empty.html');
+    $('#footer-empty-placeholder').load('../templates/footer-empty.html');
     $('#footer-placeholder').load('../templates/footer.html');
 }
 loadSkeleton();
