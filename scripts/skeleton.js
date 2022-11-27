78c847c5e5c3d398132f3b5e2c4cbc2e41fd0831
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-26 21:37:54 -0800

    Refactor navbar-back-btn-placeholder convention

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,6 +1,6 @@
 function loadSkeleton() {
     $('#navbarPlaceholder').load('../templates/nav.html');
-    $('#navbar-back-btnPlaceholder').load('../templates/nav-back-btn.html');
+    $('#navbar-back-btn-placeholder').load('../templates/nav-back-btn.html');
     $('#navbar-no-account-iconPlaceholder').load('../templates/nav-no-account-icon.html');
     $('#footerEmptyPlaceholder').load('../templates/footer-empty.html');
     $('#footerPlaceholder').load('../templates/footer.html');
