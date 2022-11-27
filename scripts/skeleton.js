a725eb4347b4be08c46ef6a4f555a9f3ad36dedc
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-26 22:43:39 -0800

    Refactor navbarPlaceholder to navbar-placeholder

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,5 +1,5 @@
 function loadSkeleton() {
-    $('#navbarPlaceholder').load('../templates/nav.html');
+    $('#navbar-placeholder').load('../templates/nav.html');
     $('#navbar-back-btn-placeholder').load('../templates/nav-back-btn.html');
     $('#navbar-no-account-iconPlaceholder').load('../templates/nav-no-account-icon.html');
     $('#footerEmptyPlaceholder').load('../templates/footer-empty.html');
