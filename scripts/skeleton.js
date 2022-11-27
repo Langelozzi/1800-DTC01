924bb1058548626651310689bf1dcbb32ac11de8
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-26 22:53:56 -0800

    Refactor navbar-no-account-icon-placeholder

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,7 +1,7 @@
 function loadSkeleton() {
     $('#navbar-placeholder').load('../templates/nav.html');
     $('#navbar-back-btn-placeholder').load('../templates/nav-back-btn.html');
-    $('#navbar-no-account-iconPlaceholder').load('../templates/nav-no-account-icon.html');
+    $('#navbar-no-account-icon-placeholder').load('../templates/nav-no-account-icon.html');
     $('#footer-empty-placeholder').load('../templates/footer-empty.html');
     $('#footer-placeholder').load('../templates/footer.html');
 }
